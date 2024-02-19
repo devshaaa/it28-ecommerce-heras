d259768c2c9c1fda7042e3afbf9c668d4f8ce232
Shaa <143495881+devshaaa@users.noreply.github.com>
2024-02-19 17:42:57 +0800

    hello

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  fetch("./data.json")
+  fetch("data.json")
     .then(response => response.json())
     .then(data => {
       const productCardsContainer = document.getElementById("product-cards");
